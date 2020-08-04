884d5d5fb5dc2a0416f95a6abd2725b890e1fc72
Lucia Brammer <brammerl96@gmail.com>
2020-08-03 19:33:30 -0700

    updating pics so they can be sized down depending on screen size

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import mainPage from '../photos/main-page.png';
+import Divider from '@material-ui/core/Divider';
+import aboutMeLogo from '../photos/about-me-logo.png';
+import profilePic from '../photos/profile-pic.png';
 
 export default class Home extends Component {
   render() {
     return (
-    <div class='main-section about-page'>
-      <img src={mainPage} id='about-me-image' alt='Lucia Brammer, full-stack engineer, biologist, artist and skater'/>
+    <div class='home-section about-page'>
+      <img src={aboutMeLogo} id='about-me-image' alt='Lucia Brammer, full-stack engineer, biologist, artist and skater'/>
+      <Divider orientation="vertical" flexItem />
+      <img src={profilePic} id='profile-pic' alt='Lucia Brammer, full-stack engineer, biologist, artist and skater'/>
     </div>
     )
   }
--- src/data/card-data.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-  title: 'Affirm.e',
-  
-}]

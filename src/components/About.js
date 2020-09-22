00d7af220f86cada0cb5e8cdda4d3265e018626d
Lucia Brammer <brammerl96@gmail.com>
2020-09-21 22:07:00 -0700

    fixing spelling

--- src/components/About.js
+++ src/components/About.js
@@ -29,7 +29,7 @@ export default class About extends Component {
                   Making something! (art, digital media, movement) 
                 </li>
                 <li>
-                  Skatboarding
+                  Skateboarding
                 </li>
               </ul>
             </div>
